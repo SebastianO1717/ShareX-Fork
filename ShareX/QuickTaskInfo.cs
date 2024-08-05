301405907191a3fa083613ea33b1c03eb6984e4f
Sebastian Hernandez <sebaoctavio@gmail.com>
2024-08-04 17:40:53 -0700

    Add IsEmpty and IsUploadTask properties to QuickTaskInfo
    Two new read-only properties were added to the QuickTaskInfo class:
    - `IsEmpty`: Returns true if AfterCaptureTasks is set to None.
    - `IsUploadTask`: Returns true if AfterCaptureTasks includes the UploadImageToHost flag.
    
    These properties provide additional functionality to determine the state of after-capture tasks.

--- ShareX/QuickTaskInfo.cs
+++ ShareX/QuickTaskInfo.cs
@@ -35,6 +35,21 @@ namespace ShareX
         public string Name { get; set; }
         public AfterCaptureTasks AfterCaptureTasks { get; set; }
         public AfterUploadTasks AfterUploadTasks { get; set; }
+        public bool IsEmpty
+        {
+            get
+            {
+                return AfterCaptureTasks == AfterCaptureTasks.None;
+            }
+        }
+
+        public bool IsUploadTask
+        {
+            get
+            {
+                return AfterCaptureTasks.HasFlag(AfterCaptureTasks.UploadImageToHost);
+            }
+        }
 
         public bool IsValid
         {
